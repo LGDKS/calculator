4a9b0a8e32fae632e31aa2050ec8a19e820a2896
Lukas <gonsenical@gmail.com>
2023-07-06 07:59:57 +0200

    add operate and basic calculator

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ const divide = function (...numbers) {
   const total = numbers.reduce((accumulator, number) => accumulator / number);
   return total;
 };
+
+/* let numberOne = 3;
+let operator = '+';
+let numberTwo = 5; */
+
+const operate = function (numberOne, operator, numberTwo) {
+  switch (operator) {
+    case '+':
+      return add(numberOne, numberTwo);
+    case '-':
+      return substract(numberOne, numberTwo);
+    case '*':
+      return multiply(numberOne, numberTwo);
+    case '/':
+      return divide(numberOne, numberTwo);
+    default:
+      console.log('DEFAULT');
+      break;
+  }
+};
